03cdcc353a40d69d9758c8df5497260f0048f4ee
Michael Qin <michael.qin@uwaterloo.ca>
2019-08-24 12:25:10 -0400

    Added increment to dynamic text instead of text field

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,17 @@
+
+
+
 function incrementValue(id, incrementAmount)
 {
-    var value = parseInt(document.getElementById(id).value, 10);
-    value = isNaN(value) ? 0 : value;
-    value = value + incrementAmount;
-    document.getElementById(id).value = value;
+    // var value = parseInt(document.getElementById(id).value, 10);
+    // value = isNaN(value) ? 0 : value;
+    // value = value + incrementAmount;
+    // document.getElementById(id).value = value;
+
+    var value = parseInt(document.getElementById(id).innerHTML, 10);
+    value++;
+    document.getElementById(id).innerHTML = value;
 }
+
+var p1=document.getElementById("p1");
+p1.innerHTML=5;
