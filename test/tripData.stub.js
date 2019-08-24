4104fbfb07e618127fbc3c9ca763002d73197e07
AlexXuChen <alex.xu.chen@gmail.com>
2019-08-24 14:28:12 -0400

    changed stub data to one entry

--- test/tripData.stub.js
+++ test/tripData.stub.js
@@ -1,180 +1,22 @@
 const stubData = {
-"destination_addresses" : [
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "339 Hicks St, Brooklyn, NY 11201, USA",
-  "585 Schenectady Ave, Brooklyn, NY 11203, USA",
-  "102-1 66th Ave, Forest Hills, NY 11375, USA",
-  "1000 N Village Ave, Rockville Centre, NY 11570, USA",
-  "324 Beach 19th St, Far Rockaway, NY 11691, USA",
-  "585 Schenectady Ave, Brooklyn, NY 11203, USA",
-  "102-1 66th Ave, Forest Hills, NY 11375, USA",
-  "1000 N Village Ave, Rockville Centre, NY 11570, USA",
-  "324 Beach 19th St, Far Rockaway, NY 11691, USA"
-],
-"origin_addresses" : [ "569 Vermont St, Brooklyn, NY 11207, USA" ],
-"rows" : [
-  {
-      "elements" : [
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "6.5 mi",
-              "value" : 10422
-            },
-            "duration" : {
-              "text" : "36 mins",
-              "value" : 2144
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "2.9 mi",
-              "value" : 4661
-            },
-            "duration" : {
-              "text" : "18 mins",
-              "value" : 1090
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "8.4 mi",
-              "value" : 13550
-            },
-            "duration" : {
-              "text" : "23 mins",
-              "value" : 1400
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "16.0 mi",
-              "value" : 25738
-            },
-            "duration" : {
-              "text" : "30 mins",
-              "value" : 1780
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "13.4 mi",
-              "value" : 21490
-            },
-            "duration" : {
-              "text" : "35 mins",
-              "value" : 2079
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "2.9 mi",
-              "value" : 4661
-            },
-            "duration" : {
-              "text" : "18 mins",
-              "value" : 1090
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "8.4 mi",
-              "value" : 13550
-            },
-            "duration" : {
-              "text" : "23 mins",
-              "value" : 1400
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "16.0 mi",
-              "value" : 25738
-            },
-            "duration" : {
-              "text" : "30 mins",
-              "value" : 1780
-            },
-            "status" : "OK"
-        },
-        {
-            "distance" : {
-              "text" : "13.4 mi",
-              "value" : 21490
-            },
-            "duration" : {
-              "text" : "35 mins",
-              "value" : 2079
-            },
-            "status" : "OK"
-        }
-      ]
-  }
-],
-"status" : "OK"
+  "destination_addresses" : [ "Victoria, BC, Canada" ],
+  "origin_addresses" : [ "Vancouver, BC, Canada" ],
+  "rows" : [
+     {
+        "elements" : [
+           {
+              "distance" : {
+                 "text" : "114 km",
+                 "value" : 114211
+              },
+              "duration" : {
+                 "text" : "3 hours 4 mins",
+                 "value" : 11013
+              },
+              "status" : "OK"
+           }
+        ]
+     }
+  ],
+  "status" : "OK"
 }
