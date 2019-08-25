aeef844887ddd78a241a8fd99502e3cf7586221b
SufiyaanNadeem <sufiyaan.nadeem@gmail.com>
2019-08-24 22:27:39 -0400

    require works

---
+++ main.js
@@ -0,0 +1,44 @@
+const timediff = require('timediff');
+
+var startTime;
+var stopTime;
+
+const calculateElapsedTime = (startTime, endTime) => {
+    const elapsedTimeRaw = timediff(startTime, endTime, 's')
+    return elapsedTimeRaw.milliseconds
+}
+
+
+function setTime(element) {
+    var thisButton = element;
+    var currentTime = new Date();
+
+    if (element.id == "startButton") {
+        console.log("Start time captured");
+        startTime = currentTime;
+    } else {
+        console.log("Stop time captured");
+        stopTime = stopTime;
+        calculateElapsedTime(startTime, stopTime);
+    }
+}
+
+
+
+const safeDrivingRate = (safeTrips, totalTrips) => {
+    return safeTrips / totalTrips
+}
+
+
+const getDate = () => {
+    return new Date()
+}
+
+
+
+module.exports = {
+    calculateElapsedTime,
+    safeDrivingRate,
+    getDate,
+    setTime
+}
